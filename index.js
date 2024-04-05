64bdbfbeb8112caf9f8fd3a9a16b566f848331a0
Amrendra Kumar <amarmca51@gmail.com>
2024-04-05 18:15:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 const array = [45, 101, -10, 555422, 3324, 543, 12, 123, 543, 33, 33, 123]
 
 
-function bubbleSort(array){
- let arr = array.slice(); 
+let arr =[3,1,2,4,6,5]
 
-for(let i =0 ; i < arr.length-1 ; i ++){
-  let isSwap = false;
-  for(j = 0 ; j < arr.length - 1 - i; j++){
-    if(arr[j] > arr[j+1]){
-      [arr[j] , arr[j+1]]  = [arr[j+1] , arr[j]]
-        isSwap = true
+for(let i=0;  i< arr.length; i++){
+    
+    for(let j = 0; j< arr.length-i-1; j++ ){
+        
+        if(arr[j] > arr[j+1]){
+            let temp = arr[j];
+            arr[j] = arr[j+1]
+            arr[j+1] = temp
+        }
     }
-  }
-   if(isSwap == false){
-     break;
-   } ;
-  
+    
 }
-return arr;
-}
+
+
+console.log(arr)
 
 
 console.log(bubbleSort(array));
